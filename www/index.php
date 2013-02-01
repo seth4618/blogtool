ed85a637895f04c33ec077409c43dc8233de517d
Seth Goldstein <seth@flashgroup.com>
2013-01-31 22:51:48 -0500

    fix some small things

--- www/index.php
+++ www/index.php
@@ -2,9 +2,9 @@
 <html>
 <head>
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-   <link rel="stylesheet" type="text/css" href="d0/scss/main.css"/>
+   <link rel="stylesheet" type="text/css" href="/d0/scss/main.css"/>
    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-   <script type="text/javascript" src="/d0/sjs/jquery.dotdotdot-1.5.6.js"></script>
+   <script type="text/javascript" src="/d0/js/jquery.dotdotdot.js"></script>
    <script src="d0/sjs/index.js"></script>
 </head>
 <body>
