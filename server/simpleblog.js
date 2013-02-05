dac58bf53dc02ebe6221c8ae0b26ea55543914d2
Seth Goldstein <seth@flashgroup.com>
2013-02-04 19:17:22 -0500

    added in a new replacement key to insert google analytics code.  Also start using Config class for configurable options.

--- server/simpleblog.js
+++ server/simpleblog.js
@@ -4,13 +4,22 @@ Util = require("./util.js");
 url = require("url");
 FileServer = require("./file.js");
 var server;
-BASEPATH = "/home/seth/blog/blogtool/www";
+Config = require('./config.js');
+Config.init('../data/config.txt');
+Config.require(['gacode', 'basepath']);
+BASEPATH = Config.getString('basepath');
 Synchronizer = require("./sync.js");
 Post = require('./post.js');
+
+var analyticsCode;
 
 // set up replacements
 FileServer.setReplacementKey('posts', {func: function(rf) { return Post.formatAll(); }, sync: 1});
 FileServer.setReplacementKey('post', {func: Post.formatPost, sync: 0});
+FileServer.setReplacementKey('ga', {func: function(rf) { return analyticsCode; }, sync: 1});
+
+// init the analytics code
+analyticsCode = ['<script type="text/javascript">var _gaq = _gaq || [];  _gaq.push([\'_setAccount\', \'', Config.getString('gacode'), '\']);  _gaq.push([\'_trackPageview\']);  (function() {    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);  })();\n</script>'].join('');
 
 try {
     /** @type {!http.Server} */ server = http.createServer(onRequest);
