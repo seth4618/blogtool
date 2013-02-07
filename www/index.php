47eb90ebe2ecc52dad1f3a6c2d4ddba9a2c26d67
Seth Goldstein <seth@flashgroup.com>
2013-02-06 22:35:37 -0500

    use three col layout, add categories

--- www/index.php
+++ www/index.php
@@ -3,13 +3,30 @@
 <head>
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
    <link rel="stylesheet" type="text/css" href="/d0/scss/main.css"/>
+   <link rel="stylesheet" type="text/css" href="/d0/scss/layout-three.css"/>
    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
    <script type="text/javascript" src="/d0/js/jquery.dotdotdot.js"></script>
    <script src="d0/sjs/index.js"></script>
 </head>
 <body>
 <div id="pagetitle">Seth's blog</div>
-{posts}
+<div class="colmask threecol">
+	<div class="colmid">
+		<div class="colleft">
+			<div class="col1">
+			  {posts}
+			</div>
+			<div class="col2">
+			</div>
+			<div class="col3">
+			  <div class="cats">
+			  Categories:<br>
+			  {cats}
+			  </div>
+			</div>
+		</div>
+	</div>
+</div>
 {ga}
 </body>
 </html>
