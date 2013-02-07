ac01bf2b0e895ff8ac422f2ef0d618a34ff9ceb3
Seth Goldstein <seth@flashgroup.com>
2013-02-07 08:33:30 -0500

    initial add

---
+++ www/cat.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <link rel="stylesheet" type="text/css" href="/d0/scss/main.css"/>
+   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+   <script type="text/javascript" src="/d0/js/jquery.dotdotdot.js"></script>
+   <script src="/d0/sjs/index.js"></script>
+</head>
+<body>
+<div id="pagetitle"><a href="/">Seth's blog</a></div>
+<div class="onepost">
+{postsbycat}
+</div>
+{ga}
+</body>
+</html>
